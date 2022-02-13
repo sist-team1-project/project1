b8c74e855cb178a2358350fb697735ea4f58c95b
eunyooung <choi327391@gmail.com>
2022-02-13 12:48:45 +0900

    Initial commit

---
+++ project1/src/main/java/sist/com/vo/HelpfulVO.java
@@ -0,0 +1,32 @@
+package sist.com.vo;
+
+public class HelpfulVO {
+    
+    private int helpful_id;
+    private String u_id;
+    private int review_id;
+
+    public int getHelpful_id() {
+        return helpful_id;
+    }
+
+    public void setHelpful_id(int helpful_id) {
+        this.helpful_id = helpful_id;
+    }
+
+    public String getU_id() {
+        return u_id;
+    }
+
+    public void setU_id(String u_id) {
+        this.u_id = u_id;
+    }
+
+    public int getReview_id() {
+        return review_id;
+    }
+
+    public void setReview_id(int review_id) {
+        this.review_id = review_id;
+    }
+}
