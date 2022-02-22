4ef3b15d0be6d2fe01ece4572cbbc433b49191bf
kimseung123 <sk00021@naver.com>
2022-02-23 00:08:34 +0900

    companySearchBar.jsp & CompanySearchModel.java 수정

--- project1/src/main/java/sist/com/model/CompanySearchModel.java
+++ project1/src/main/java/sist/com/model/CompanySearchModel.java
@@ -13,7 +13,30 @@ public class CompanySearchModel {
 	@RequestMapping("search/companysearchBar.do")
     public String main_page(HttpServletRequest request) {
 
+		CompanyDAO c = new CompanyDAO();
+        AdDAO a = new AdDAO();
+        ReviewDAO r = new ReviewDAO();
+        PostDAO p = new PostDAO();
+		
         request.setAttribute("main_jsp", "../search/companySearchBar.jsp");
+        
+        List<CompanyVO> company = c.bestCompanyList();
+        List<String> review = new ArrayList<String>();
+
+        for (int i = 0; i < company.size(); i++) {
+            String bestreview = r.bestCompanyReviewList(company.get(i).getC_id());
+            
+            // 리뷰가 없을 경우
+            if (bestreview.isEmpty()) {
+                bestreview = "유저들의 리뷰를 기다리고 있습니다.";
+            }
+            review.add(bestreview);
+        }
+        request.setAttribute("company", company);
+        request.setAttribute("review", review);
+        
+
+        
         return "../main/main.jsp";
     }
 }
